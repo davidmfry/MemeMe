e1699a246d5b18a61039a677f271c7e4118131c6
David Fry <david.fry.tv@gmail.com>
2015-03-10 12:07:42 -0700

    Camera and album buttons working

--- MemeMe/MemeMeEditor.swift
+++ MemeMe/MemeMeEditor.swift
@@ -8,15 +8,89 @@
 
 import UIKit
 
-class MemeMeEditor: UIViewController
+class MemeMeEditor: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
 {
 
+    @IBOutlet weak var topTextfield: UITextField!
+    @IBOutlet weak var bottomTextfield: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraBarButton: UIBarButtonItem!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
+ 
+    }
+    
+    override func viewDidAppear(animated: Bool)
+    {
+        if !checkForCamera()
+        {
+            self.cameraBarButton.enabled = false
+        }
     }
 
-
+//MARK: Delegate Methods
+    
+    
+//MARK: IBActions
+    @IBAction func actionButtonPressed(sender: UIBarButtonItem)
+    {
+    }
+    
+    @IBAction func cancelButtonPressed(sender: UIBarButtonItem)
+    {
+        
+    }
+    
+    @IBAction func cameraButtonPressed(sender: UIBarButtonItem)
+    {
+        self.presentImagePicker("camera")
+    }
+    
+    @IBAction func albumButtonPressed(sender: UIBarButtonItem)
+    {
+        self.presentImagePicker("photo-libary")
+    }
+//MARK: Helper function
+    func checkForCamera() -> Bool
+    {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+        {
+            return true
+        }
+        else
+        {
+            println("No device found")
+            return false
+        }
+    }
+    
+    func presentImagePicker(pickerType: String)
+    {
+        
+        if self.checkForCamera() && pickerType == "camera"
+        {
+            self.presentViewController(self.createImageController(.Camera), animated: true, completion: nil)
+        }
+        else if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) && pickerType == "photo-libary"
+        {
+            self.presentViewController(self.createImageController(.PhotoLibrary), animated: true, completion: nil)
+        }
+    }
+    
+    func createImageController(sourceType: UIImagePickerControllerSourceType) -> UIImagePickerController
+    {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = sourceType
+        imagePicker.delegate = self
+        return imagePicker
+    }
+    
+    
+    
+    
+    
 }
 
