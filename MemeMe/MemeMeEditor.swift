869cf32a5dc62d7a0e0c362d4af4c3af106a5e7f
David Fry <david.fry.tv@gmail.com>
2015-03-10 10:14:32 -0700

    Meeme editor layout done

---
+++ MemeMe/MemeMeEditor.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by David on 3/10/15.
+//  Copyright (c) 2015 David Fry. All rights reserved.
+//
+
+import UIKit
+
+class MemeMeEditor: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
